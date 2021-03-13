19757c4dd8a26715a22b7e9eed7861985159e85e
mhsidd997 <muhammedhamzasiddiqui@gmail.com>
2021-03-14 00:14:43 +0500

    Updates

---
+++ precache-manifest.9e356847b1a99c115c7521d3f6f1b1f5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "303c9e97376e6724fbd041611cdb41c5",
+    "url": "/Dashboard_React_App/index.html"
+  },
+  {
+    "revision": "ce6d2047c41631762633",
+    "url": "/Dashboard_React_App/static/css/2.ee241de9.chunk.css"
+  },
+  {
+    "revision": "a470c9f14c5759e3daf0",
+    "url": "/Dashboard_React_App/static/css/main.55238172.chunk.css"
+  },
+  {
+    "revision": "ce6d2047c41631762633",
+    "url": "/Dashboard_React_App/static/js/2.edfd7c45.chunk.js"
+  },
+  {
+    "revision": "ab96ce05e716d68263e2c8b2e6ec00d9",
+    "url": "/Dashboard_React_App/static/js/2.edfd7c45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a470c9f14c5759e3daf0",
+    "url": "/Dashboard_React_App/static/js/main.f4d5eb6a.chunk.js"
+  },
+  {
+    "revision": "e43a7d1999329a1971a7",
+    "url": "/Dashboard_React_App/static/js/runtime-main.93b06f47.js"
+  }
+]);
